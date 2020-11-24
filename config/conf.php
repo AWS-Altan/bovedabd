0746bf07bb524f509492f8d0193a036b15c8ffac
Jose Luis Duran <josel.duran.2010@gmail.com>
2020-11-24 17:34:32 -0600

    2020-11-24 apuntado de base a producción 5

--- config/conf.php
+++ config/conf.php
@@ -2,11 +2,11 @@
 
 return [
 	'url_miniature' => 'http://elb-pacific-media-749655226.us-east-1.elb.amazonaws.com:8000/storage/',
-	'url_360' 		=> 'https://altanredes-prod.apigee.net/cm-360/v1/',
-	'url_operation' => 'https://altanredes-prod.apigee.net/cm/v1/',
-	'url_login' 	=> 'https://altanredes-prod.apigee.net/v1/oauth/accesstoken?grant-type=client_credentials',
-	'url_sqm' 		=> 'https://altanredes-prod.apigee.net/sqm/v1/',
-	'url_ac' 		=> 'https://altanredes-prod.apigee.net/ac/v1/',
-	'url_batch'     => 'https://altanredes-prod.apigee.net',
-	'url_cm_coppel' => 'https://altanredes-prod.apigee.net/cm/v1/',
+	'url_360' 		=> 'https://altanredes-test.apigee.net/cm-360/v1/',
+	'url_operation' => 'https://altanredes-test.apigee.net/cm/v1/',
+	'url_login' 	=> 'https://altanredes-test.apigee.net/v1/oauth/accesstoken?grant-type=client_credentials',
+	'url_sqm' 		=> 'https://altanredes-test.apigee.net/sqm/v1/',
+	'url_ac' 		=> 'https://altanredes-test.apigee.net/ac/v1/',
+	'url_batch'     => 'https://altanredes-test.apigee.net',
+	'url_cm_coppel' => 'https://altanredes-test.apigee.net/cm/v1/',
 ];
