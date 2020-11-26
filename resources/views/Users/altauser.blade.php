db389d5bf5e182d46fe3f0715af4462b4ff331fd
Jose Luis Duran <josel.duran.2010@gmail.com>
2020-11-26 15:08:09 -0600

    2020-11-26 prueba de route 8

--- resources/views/Users/altauser.blade.php
+++ resources/views/Users/altauser.blade.php
@@ -11,6 +11,37 @@
 					<div id="example-basic">
 						<h3><span class="head-font capitalize-font">Alta de usuario</span></h3>
 						<section>
+                            <form id="form_tabs" action="#">
+                                <div class="panel panel-default">
+                                    <!-- Header Subseccion -->
+                                    <div class="panel-heading">
+    								Datos de la cuenta (usuario)
+                                    </div>
+                                    <!-- despues del header de la seccion -->
+                                    <div class="card-body">
+                                        <div class="row">
+                                        </div>
+                                        <div class="row">
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /panel1 -->
+                                <div class="panel panel-default">
+                                    <!-- Header Subseccion -->
+                                    <div class="panel-heading">
+    								Header sub sec 2
+                                    </div>
+                                </div>
+
+                                <!-- /panel2 -->
+                                <div class="panel panel-default">
+                                    <!-- Header Subseccion -->
+                                    <div class="panel-heading">
+    								Header sub sec 3
+                                    </div>
+                                </div>
+                            </form>
+
 							<div class="row">
 								<div class="col-sm-12">
 									<div class="form-group mb-0">
@@ -18,7 +49,7 @@
 											<div class="col-sm-9">
 												<div class="form-wrap" style="display: inline-block;">
 													    <div class="help-block" style="color:#9E1D22; font-weight: bold;">
-													    	Alta de usuario
+													    	texto informativo
 													    </div>
 													</div>
 											</div>
